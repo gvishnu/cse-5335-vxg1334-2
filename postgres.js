b2f33e1b461df063a2f9a0caf564e29afd1b16f3
gvishnu <gvishnukiran@gmail.com>
2015-11-18 00:45:16 +0000

    add2

--- postgres.js
+++ postgres.js
@@ -14,7 +14,7 @@ client.connect(function(err, client){
     if (err) throw err;
     });
     
-    var query2 = client.query('CREATE TABLE flowers1(id VARCHAR(15), a VARCHAR(40), b VARCHAR(40), c VARCHAR(40), d VARCHAR(40))');
+    var query2 = client.query('CREATE TABLE flowers2(id VARCHAR(15), a VARCHAR(40), b VARCHAR(40), c VARCHAR(40), d VARCHAR(40))');
     query2.on('end', function(){
     var csvfile = 'data.csv';
     var csv_con = new converter();
@@ -24,7 +24,7 @@ client.connect(function(err, client){
         
         for(var i = 0; i<jsonObj.length;i++){
         
-        client.query('INSERT INTO flowers1(id,a,b,c,d) VALUES ($1,$2,$3,$4,$5)', 
+        client.query('INSERT INTO flowers2(id,a,b,c,d) VALUES ($1,$2,$3,$4,$5)', 
         [jsonObj[i].id, jsonObj[i].a, jsonObj[i].b,jsonObj[i].c,jsonObj[i].d],function(err){
             
             if(err) throw err;
@@ -38,7 +38,7 @@ client.connect(function(err, client){
          
           if (err) throw err; 
           
-          var query = client.query('SELECT a FROM flowers1 WHERE id = $1',[result.P_Key]);
+          var query = client.query('SELECT a FROM flowers2 WHERE id = $1',[result.P_Key]);
               
               query.on('row', function(row, result){
             result.addRow(row);
@@ -50,7 +50,7 @@ client.connect(function(err, client){
          });
          
           
-          var query1 = client.query('SELECT a FROM flowers1 WHERE b = $1',[result.S_key]);
+          var query1 = client.query('SELECT a FROM flowers2 WHERE b = $1',[result.S_key]);
           
           query1.on('row', function(row1, result1){
             result1.addRow(row1);
